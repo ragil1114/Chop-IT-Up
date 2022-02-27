75cd685db93b9e738b884b835c0436121aa006ac
Jeremy O'Daniel <jeremy.odaniel@gmail.com>
2022-02-26 22:30:46 -0600

    more...

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,7 @@
-<<<<<<< HEAD
-// import the Sequelize constructor from the library
-=======
->>>>>>> 1fc25328be85b74b5372923267b48179a0657dd8
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-<<<<<<< HEAD
-
-// create connection to our database, pass in your MySQL information for 
-// username and password
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, 
-                                process.env.DB_PW, {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3001
-});
-
-=======
 // create connection to our db
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
@@ -26,6 +10,5 @@ const sequelize = process.env.JAWSDB_URL
       dialect: 'mysql',
       port: 3306
     });
->>>>>>> 1fc25328be85b74b5372923267b48179a0657dd8
 
 module.exports = sequelize;
