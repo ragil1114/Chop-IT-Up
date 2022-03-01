2ff536aa9d111608c87508d278bff40b2a379806
Ragil14 <rahil.gilani@gmail.com>
2022-03-01 00:39:44 -0600

    import socket.io modules

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const http = require('http');
+const server = http.createServer(app);
+
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
