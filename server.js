54b1dc50bb86465280f9ccdd1cae7a3df5ed0867
unknown <steinway.nathan@gmail.com>
2022-03-04 15:49:41 -0600

    added comments

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Create an HTTP server taking app (defined as express(); above), then set it to variable called "server".
 const http = require('http');
 const server = http.createServer(app);
 
+// Set Server object to require socket.io, then define io as a new Server object that passes "server" variable as argument.
+// This finalizes the connection between express and socket.io. Socket.io can now use express server as it's base of operations.
 const { Server } = require("socket.io");
 const io = new Server(server);
 
@@ -40,9 +43,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
 
+// io calls socket.io.
+// io.on calls the following function when a socket.io session is made
+// connection is the event and socket is what's created when the event goes through
 io.on('connection', (socket) => {  
   
   console.log('a user connected');
+
+  // as such, socket.on is used here instead of io.on
 
   socket.on('disconnect', () => {    
     console.log('user disconnected');  
@@ -60,6 +68,10 @@ io.on('connection', (socket) => {
 });
 
 // "Normal" HTTP requests are handled using Express.js and other middleware
+// To finalize initialization of Socket.IO, app.listen was changed to server.listen. 
+// This is because further up in the document we made it such that const server = http.createServer(app);
 sequelize.sync({ force: false }).then(() => {
   server.listen(PORT, () => console.log('Now listening'));
 });
+
+// socket.io can now use the application. see chatbox.handlebars for further socket.io functionality.
